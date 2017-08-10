e16b7accb3f19c6ec63b4c3b2570e8d90b627831
Qian JX <raistlinqjx@gmail.com>
2017-08-10 11:17:39 +0800

    Initial commit

---
+++ GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  GameDemo
+//
+
+//  Copyright (c) 2015年 zfsoft. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  GameDemo
+//
+//  Created by Qian JX on 15/11/25.
+//  Copyright © 2015年 zfsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
